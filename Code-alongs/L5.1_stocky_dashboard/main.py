efd2f56d6fff670497af4c4bf473e0330112b3a2
Johan Sandberg <jvesand@gmail.com>
2022-11-17 11:16:38 +0100

    refactored into separate layout module

--- Code-alongs/L5.1_stocky_dashboard/main.py
+++ Code-alongs/L5.1_stocky_dashboard/main.py
@@ -7,6 +7,7 @@ from dash.dependencies import Output, Input
 import plotly_express as px
 from time_filtering import filter_time
 import pandas as pd
+from layout import Layout
 
 directory_path = os.path.dirname(__file__)
 path = os.path.join(directory_path, "stocks_data")
@@ -19,25 +20,13 @@ stock_data_object = StockData(path)
 # print(stock_data_object.stock_dataframe("AAPL"))
 
 symbol_dict = {"AAPL": "Apple", "NVDA": "Nvidia", "TSLA": "Tesla", "IBM": "IBM"}
+
 # create dictionary of dataframes (dictionary of lists of dataframes)
 df_dict = {
     symbol: stock_data_object.stock_dataframe(symbol) for symbol in symbol_dict
 }  # dict loop takes keys, if want values do .items()
 
-stock_options_dropdown = [
-    {"label": name, "value": symbol} for symbol, name in symbol_dict.items()
-]
 
-ohlc_options = [
-    {"label": option, "value": option} for option in ("open", "high", "low", "close")
-]
-
-slider_marks = {
-    i: mark
-    for i, mark in enumerate(
-        ["1 day", "1 week", "1 month", "3 months", "1 year", "5 years", "Max"]
-    )
-}
 print(df_dict.keys())
 # print(df_dict["TSLA"]) #list with two dataframes
 # print(df_dict["TSLA"][0]) #a dataframe
@@ -45,26 +34,7 @@ print(df_dict.keys())
 # create a Dash App
 app = dash.Dash(__name__)
 
-app.layout = html.Main(
-    [
-        html.H1("Techy stocks viewer"),
-        html.P("Choose a stock"),
-        dcc.Dropdown(
-            id="stockpicker-dropdown",
-            options=stock_options_dropdown,
-            value="AAPL",
-        ),
-        html.P(id="highest-value"),
-        html.P(id="lowest-value"),
-        dcc.RadioItems(id="ohlc-radio", options=ohlc_options, value="close"),
-        dcc.Graph(id="stock-graph"),
-        dcc.Slider(
-            id="time-slider", min=0, max=6, marks=slider_marks, value=2, step=None
-        ),
-        # storing intermediate value on clients browser in order to share between several callbacks
-        dcc.Store(id="filtered_df"),
-    ]
-)
+app.layout = Layout(symbol_dict).layout()
 
 # now have a filtered_df that can be used as Input
 @app.callback(
